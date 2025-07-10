4bee6c3bc825d10c5e77653d7ce21c0747164024
sethulakshmy24 <sethu29286@gmail.com>
2025-07-10 14:06:27 +0530

    server changes

--- server.js
+++ server.js
@@ -19,6 +19,25 @@ const mimeTypes = {
 };
 
 const server = http.createServer((req, res) => {
+  // Set CORS headers for cross-origin requests with cookie support
+  const origin = req.headers.origin;
+  if (origin) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  } else {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Cookie, Set-Cookie');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Expose-Headers', 'Set-Cookie');
+  
+  // Handle preflight OPTIONS requests
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+
   let filePath = req.url === '/' ? '/index.html' : req.url;
   filePath = filePath.split('?')[0]; // Remove query params
   const ext = path.extname(filePath);
